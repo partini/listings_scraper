d14e046cefa467e67a6139b8a4bfbf4f41bcbe30
Partin <partin@labreches-MacBook-Pro.local>
2015-07-26 18:27:52 +0200

    Code clean.

--- run.py
+++ run.py
@@ -6,34 +6,47 @@ import requests
 response = requests.get('http://www.merrjep.com/list.aspx?municipality=124')
 
 soup = BeautifulSoup(response.text, "html.parser")
-orat = soup.select('th')
-table_body = soup.select('.listing_thumbs')
-
-rows = soup.find('.listing_thumbs')
 
 def main():
-    table = soup.find("table",{"class": "listing_thumbs"})
-    for row in table.findAll("tr"):
+    listing_results_table = soup.find("table",{"class": "listing_thumbs"})
+    for row in listing_results_table.findAll("tr"):
         cell = row.select("a")
 
         if len(cell) > 0:
-            responsi = requests.get('http://www.merrjep.com/' + cell[0].get('href'))
-            soupi = BeautifulSoup(responsi.text, "html.parser")
-            description = soupi.select('.auctionDescriptionContent')
-            print "Pershkrimi:" + description[0].getText()
+            single_link_result = requests.get('http://www.merrjep.com/' + cell[0].get('href'))
+            single_link_parsed = BeautifulSoup(single_link_result.text, "html.parser")
+            description = single_link_parsed.select('.auctionDescriptionContent')[0].getText()
 
-            contact_name = soupi.find('span', id="ctl00_ContentPlaceHolder1_TabContainer1_TabPanel1_ContactNameLabel")
-            print contact_name.getText()
+            contact_name = single_link_parsed.find('span', id="ctl00_ContentPlaceHolder1_TabContainer1_TabPanel1_ContactNameLabel").getText()
 
-            phone = soupi.find('span', id="ctl00_ContentPlaceHolder1_TabContainer1_TabPanel1_ContactTelephoneLabel")
-            print phone.getText()
 
-            titulli = soupi.find('span', id="ctl00_ContentPlaceHolder1_TopHeadlineLabel")
-            print titulli.getText()
+            phone = single_link_parsed.find('span', id="ctl00_ContentPlaceHolder1_TabContainer1_TabPanel1_ContactTelephoneLabel").getText()
 
-            qmimi = soupi.find('span', id="ctl00_ContentPlaceHolder1_TabContainer1_TabPanel1_AdPriceLabel")
-            if qmimi != None:
-                print qmimi.getText()
+
+            tittle = single_link_parsed.find('span', id="ctl00_ContentPlaceHolder1_TopHeadlineLabel").getText()
+
+            price = ""
+            try:
+                price = single_link_parsed.find('span', id="ctl00_ContentPlaceHolder1_TabContainer1_TabPanel1_AdPriceLabel").getText()
+            except:
+                price = ""
+
+            large_image = ""
+            try:
+                large_image = "http://merrjep.com/" + single_link_parsed.find('img', id="ctl00_ContentPlaceHolder1_TabContainer1_TabPanel1_LargeImage")['src']
+            except:
+                large_image = ""
+
+            result = {
+                "contact_name": contact_name,
+                "phone": phone,
+                "titulli": tittle,
+                "description": description,
+                "qmimi": price,
+                "imazhi": large_image
+            }
+            print result
+
             print "________________________________________________"
 
 if __name__ == "__main__":
